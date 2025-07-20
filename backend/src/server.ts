5f237c9c582b32cc0882d43b04c9a256277b4f6e
SAY10s <nikodemmajzner@gmail.com>
2025-07-20 21:07:08 +0200

    FEAT: attacking players (yet without actually dealing damage)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,31 +2,53 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import { PrismaClient } from "./generated/prisma/client";
-import { Dice, DiceRoll } from "@dice-roller/rpg-dice-roller";
+import { DiceRoll } from "@dice-roller/rpg-dice-roller";
 
 const prisma = new PrismaClient();
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, { cors: { origin: "*" } });
 
-interface gameState {
+interface Player {
+  socketID: string;
+  name: string;
+  maxHP: number;
+  currentHP: number;
+}
+
+interface GameState {
+  players: Player[];
   lastAction: {
+    actor: string;
+    target: string;
     name: string;
     damage: string;
   };
+  debugMessage: string;
 }
 
-const gameState = {
+const gameState: GameState = {
+  players: [],
   lastAction: {
+    actor: "",
+    target: "",
     name: "start",
     damage: "0d0",
   },
+  debugMessage: "",
 };
 // Socket.io
 io.on("connection", (socket) => {
   console.log("Użytkownik podłączony");
 
-  socket.emit("updateGameState", gameState);
+  gameState.players.push({
+    socketID: socket.id,
+    name: "taco",
+    maxHP: 100,
+    currentHP: 100,
+  });
+
+  io.emit("updateGameState", gameState);
 
   socket.on("playerAction", (action) => {
     console.log("Akcja gracza:", action);
@@ -34,14 +56,20 @@ io.on("connection", (socket) => {
     const damageRoll = new DiceRoll(action.damage);
 
     gameState.lastAction = {
-      name: action.name,
+      ...action,
       damage: damageRoll.total.toString(),
     };
+    gameState.debugMessage = damageRoll.rolls.toString();
     io.emit("updateGameState", gameState);
+  });
+
+  socket.on("disconnect", () => {
+    gameState.players = gameState.players.filter(
+      (player) => player.socketID != socket.id
+    );
   });
 });
 
-// Uruchom serwer
 httpServer.listen(3001, () => {
-  console.log("Serwer działa na http://localhost:3001");
+  console.log("Server is running on http://localhost:3001");
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,31 +4,52 @@ import { io } from "socket.io-client";
 
 const socket = io("http://localhost:3001");
 
-interface gameState {
+interface Player {
+  socketID: string;
+  name: string;
+  maxHP: number;
+  currentHP: number;
+}
+
+interface GameState {
+  players: Player[];
   lastAction: {
+    actor: string;
+    target: string;
     name: string;
     damage: string;
   };
+  debugMessage: string;
 }
 
 export default function App() {
-  const [gameState, setGameState] = useState<gameState>({
+  const [gameState, setGameState] = useState<GameState>({
+    players: [],
     lastAction: {
+      actor: "",
+      target: "",
       name: "start",
       damage: "0d0",
     },
+    debugMessage: "",
   });
 
   useEffect(() => {
-    socket.on("updateGameState", (gameState: gameState) => {
+    socket.on("updateGameState", (gameState: GameState) => {
       setGameState(gameState);
     });
   }, []);
 
-  const handleAttack = () => {
+  const handleAttack = (target: string) => {
     const numberOfDice: number = Math.floor(Math.random() * 5) + 1;
     const damage = `${numberOfDice}d6`;
-    socket.emit("playerAction", { name: "Atak mieczem", damage: damage });
+    const attackData = {
+      actor: socket.id,
+      target: target,
+      name: "Atak mieczem",
+      damage: damage,
+    };
+    socket.emit("playerAction", attackData);
   };
 
   return (
@@ -36,14 +57,35 @@ export default function App() {
       <h1 className="text-3xl font-bold text-center mb-4 text-gray-800">
         Walka Wiedźmina
       </h1>
-      <button
-        onClick={handleAttack}
-        className="block w-full py-2 px-4 bg-red-600 hover:bg-red-700 text-white font-bold rounded transition-colors mb-4"
-      >
-        Atakuj
-      </button>
       <div className="text-center text-lg text-gray-700">
-        {gameState.lastAction.name} zadał {gameState.lastAction.damage}
+        {gameState.lastAction.actor} zadał {gameState.lastAction.damage}{" "}
+        graczowi {gameState.lastAction.target}
+      </div>
+      <div className="text-center text-lg text-gray-700">
+        {gameState.debugMessage}
+      </div>
+      <div className="p-2">
+        {gameState.players.map((player) => {
+          return (
+            <div>
+              <div>
+                {player.name} - {player.socketID}{" "}
+                {player.socketID === socket.id && "(That's you!)"}
+              </div>
+              <div>
+                {player.currentHP}/{player.maxHP}
+              </div>
+              <button
+                onClick={() => {
+                  handleAttack(player.socketID);
+                }}
+                className="block w-full py-2 px-4 bg-red-600 hover:bg-red-700 text-white font-bold rounded transition-colors mb-4"
+              >
+                Atak
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
