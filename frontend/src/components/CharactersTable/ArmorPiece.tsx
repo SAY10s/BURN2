0a370eec3bb0f353fd65fe4fa2ca62aec646bba3
SAY10s <nikodemmajzner@gmail.com>
2025-07-29 22:23:15 +0200

    FEAT: made CharactersTable layout up to date with current Character interface

--- frontend/src/components/CharactersTable/ArmorPiece.tsx
+++ frontend/src/components/CharactersTable/ArmorPiece.tsx
@@ -9,7 +9,21 @@ interface ArmorPieceProps {
 export default function ArmorPiece({ armorPiece, armorPart }: ArmorPieceProps) {
   return (
     <div className="bg-gray-50 rounded-lg shadow-md p-4">
-      <h3 className="text-lg font-medium text-gray-800">{armorPart}</h3>
+      <h3 className="text-lg font-medium text-gray-800">
+        {armorPart}
+        {Object.entries(armorPiece.reductions).map(
+          ([damageType, reduction]) => {
+            if (reduction) {
+              return (
+                <span key={damageType} className="text-sm text-gray-600 px-4">
+                  <span className="text-gray-800">{damageType}:</span>{" "}
+                  <span>{reduction}%</span>
+                </span>
+              );
+            }
+          }
+        )}
+      </h3>
       <div className="mt-2 space-y-1">
         <ValueBar
           current={armorPiece.currentSP}
@@ -17,25 +31,11 @@ export default function ArmorPiece({ armorPiece, armorPart }: ArmorPieceProps) {
           gamemasterView={true}
           isPlayer={true}
         />
-        <p className="text-sm text-gray-600">
+        {/* <p className="text-sm text-gray-600">
           <strong className="text-gray-800">Encumbrance Value:</strong>{" "}
           {armorPiece.encumbranceValue}
-        </p>
+        </p> */}
       </div>
-      <ul className="mt-2 space-y-1">
-        {Object.entries(armorPiece.reductions).map(
-          ([damageType, reduction]) => {
-            if (reduction) {
-              return (
-                <li key={damageType} className="text-sm text-gray-600">
-                  <span className="text-gray-800">{damageType}:</span>{" "}
-                  <span>{reduction}</span>
-                </li>
-              );
-            }
-          }
-        )}
-      </ul>
     </div>
   );
 }
--- frontend/src/components/CharactersTable/CharactersTable.tsx
+++ frontend/src/components/CharactersTable/CharactersTable.tsx
@@ -20,114 +20,134 @@ export default function CharacterTable({
   gameMasterView,
 }: CharacterTableProps) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+    <div className="grid grid-cols-2 gap-6 mt-6">
       {characters.map((character: Character) => (
         <div
           key={character.id}
-          className="bg-white rounded-lg shadow-md p-4 hover:bg-gray-50 transition-colors"
+          className="bg-white rounded-lg shadow-md p-4 hover:bg-gray-50 transition-colors grid grid-rows-auto gap-4"
         >
-          {/* Character Info */}
+          {/* Character Info and Bars */}
           <div
-            className="cursor-pointer"
-            onClick={() => {
-              chooseCharacter(character.id);
-            }}
+            className="grid grid-cols-2"
+            style={{ gridTemplateColumns: "30% 70%" }}
           >
-            <div className={`font-medium`}>
-              {clientsCharacterId === character.id ? (
-                <span className="font-black">→{character.name}</span>
-              ) : (
-                character.name
-              )}
-            </div>
-            <div className="text-xs text-gray-500 font-mono mt-1">
-              ID: {character.id}
-            </div>
-          </div>
-
-          {/* Player/NPC Status */}
-          <div className="mt-2">
-            <span
-              className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium ${
-                character.isPlayer
-                  ? "bg-blue-100 text-blue-800"
-                  : "bg-purple-100 text-purple-800"
-              }`}
-            >
-              {character.isPlayer ? "Player" : "NPC"}
-            </span>
-          </div>
-
-          {/* Status Indicators */}
-          <div className="mt-4">
-            <div className="flex space-x-2">
-              {character.status.isBleeding && (
-                <span className="text-red-500" title="Bleeding">
-                  🩸
-                </span>
-              )}
-              {character.status.isBurning && (
-                <span className="text-orange-500" title="Burning">
-                  🔥
-                </span>
-              )}
-              {character.status.isPoisoned && (
-                <span className="text-green-500" title="Poisoned">
-                  🧪
-                </span>
-              )}
-            </div>
-          </div>
-
-          {/* Health */}
-          <div className="mt-4">
-            <ValueBar
-              current={character.currentHP}
-              max={character.maxHP}
-              gamemasterView={gameMasterView}
-              isPlayer={character.isPlayer}
-            />
-          </div>
-
-          {/* Armor */}
-          {(gameMasterView || character.isPlayer) && (
-            <div className="mt-4 grid grid-cols-2 gap-4">
-              {Object.entries(character.characterArmor).map(
-                ([armorPart, armorPiece]) => (
-                  <div key={armorPart} className="mt-2">
-                    <ArmorPiece armorPiece={armorPiece} armorPart={armorPart} />
-                  </div>
-                )
-              )}
-            </div>
-          )}
-          <div className="mt-4">
-            <StatsTable
-              stats={character.stats}
-              gamemasterView={gameMasterView}
-              isPlayer={character.isPlayer}
-            />
-          </div>
-
-          {/* Skills - Dynamically rendered */}
-          <div className="mt-4">
-            <SkillsTable
-              skills={character.skills}
-              gamemasterView={gameMasterView}
-              isPlayer={character.isPlayer}
-            />
-          </div>
-
-          {/* Attack Button */}
-          <div className="mt-4">
-            <button
-              className="flex items-center justify-center cursor-pointer"
+            <div
+              className="cursor-pointer flex flex-col items-center justify-center"
               onClick={() => {
-                attackCharacter(character.id);
+                chooseCharacter(character.id);
               }}
             >
-              <img src="/fight-icon.png" alt="Attack" className="w-8 h-8" />
-            </button>
+              <div className={`font-medium text-lg`}>
+                {clientsCharacterId === character.id ? (
+                  <span className="font-black">→{character.name}</span>
+                ) : (
+                  character.name
+                )}
+                <span
+                  className={`ml-1 inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium ${
+                    character.isPlayer
+                      ? "bg-blue-100 text-blue-800"
+                      : "bg-purple-100 text-purple-800"
+                  }`}
+                >
+                  {character.isPlayer ? "Player" : "NPC"}
+                </span>
+              </div>
+              <div className="text-xs text-gray-500 font-mono mt-1">
+                ID: {character.id}
+              </div>
+            </div>
+
+            <div className="flex flex-col space-y-2">
+              {/* Status Indicators */}
+              <div>
+                <div className="flex space-x-2 justify-center text-lg">
+                  {character.status.isBleeding && (
+                    <span className="text-red-500" title="Bleeding">
+                      🩸
+                    </span>
+                  )}
+                  {character.status.isBurning && (
+                    <span className="text-orange-500" title="Burning">
+                      🔥
+                    </span>
+                  )}
+                  {character.status.isPoisoned && (
+                    <span className="text-green-500" title="Poisoned">
+                      🧪
+                    </span>
+                  )}
+                </div>
+              </div>
+              {/* Health */}
+              <ValueBar
+                current={character.currentHP}
+                max={character.maxHP}
+                gamemasterView={gameMasterView}
+                isPlayer={character.isPlayer}
+              />
+              {/* Stamina (Mocked Data) */}
+              <ValueBar
+                current={50} // Mocked current stamina
+                max={100} // Mocked max stamina
+                gamemasterView={gameMasterView}
+                isPlayer={character.isPlayer}
+              />
+            </div>
+          </div>
+
+          {/* Collapsible Section */}
+          <details className="mt-4">
+            <summary className="cursor-pointer text-center font-medium">
+              More Details
+            </summary>
+            {/* Armor */}
+            {(gameMasterView || character.isPlayer) && (
+              <div className="grid grid-cols-2 gap-4 mt-4">
+                {Object.entries(character.characterArmor).map(
+                  ([armorPart, armorPiece]) => (
+                    <div key={armorPart}>
+                      <ArmorPiece
+                        armorPiece={armorPiece}
+                        armorPart={armorPart}
+                      />
+                    </div>
+                  )
+                )}
+              </div>
+            )}
+
+            {/* Stats and Skills */}
+            <div className="grid grid-cols-2 gap-4 mt-4">
+              <div>
+                <StatsTable
+                  stats={character.stats}
+                  gamemasterView={gameMasterView}
+                  isPlayer={character.isPlayer}
+                />
+              </div>
+              <div>
+                <SkillsTable
+                  skills={character.skills}
+                  gamemasterView={gameMasterView}
+                  isPlayer={character.isPlayer}
+                />
+              </div>
+            </div>
+
+            {/* Attack Button */}
+          </details>
+          <div>
+            <div className="flex justify-center mt-4">
+              <button
+                className="flex items-center justify-center cursor-pointer bg-red-500 hover:bg-red-600 text-white rounded-full p-2"
+                onClick={() => {
+                  attackCharacter(character.id);
+                }}
+              >
+                ATTACK{" "}
+              </button>
+            </div>
           </div>
         </div>
       ))}
