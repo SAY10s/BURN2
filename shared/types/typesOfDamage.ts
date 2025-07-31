66e3424b192a6c6cf9b4ff17979f0719c9f9cef0
SAY10s <nikodemmajzner@gmail.com>
2025-07-31 19:17:58 +0200

    FIX: update TypeScript directive to use ts-ignore instead of expect error (as it doesn't throw an error after copying to final destination)

--- shared/types/typesOfDamage.ts
+++ shared/types/typesOfDamage.ts
@@ -1,4 +1,4 @@
-//@ts-expect-error this code is valid after copying its to its final destination. This file itself is never used.
+//@ts-ignore this code is valid after copying its to its final destination. This file itself is never used.
 export enum TypesOfDamage {
   SLASHING = "slashing",
   PIERCING = "piercing",
