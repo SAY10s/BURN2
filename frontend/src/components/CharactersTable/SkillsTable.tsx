b913569b9955171db9ccc35f03df9506016e6a18
Nikodem Majzner <81244722+SAY10s@users.noreply.github.com>
2025-08-07 02:05:21 +0200

    FIX: typo in skillstable and statstable

--- frontend/src/components/CharactersTable/SkillsTable.tsx
+++ frontend/src/components/CharactersTable/SkillsTable.tsx
@@ -4,7 +4,7 @@ interface SkillsTableProps {
   skills: Skills;
   className?: string;
   showCategory?: boolean;
-  gamemasterView: boolean;
+  gameMasterView: boolean;
   isPlayer: boolean;
 }
 
@@ -12,28 +12,28 @@ export default function SkillsTable({
   skills,
   className = "",
   showCategory = false,
-  gamemasterView,
+  gameMasterView,
   isPlayer,
 }: SkillsTableProps) {
   return (
-    <div className={`space-y-2 ${className} grid grid-cols-2 gap-x-12 `}>
+    <div className={`space-y-2 ${className} grid grid-cols-2 gap-x-12`}>
       {Object.entries(skills).flatMap(([category, skillGroup]) =>
         Object.entries(skillGroup).map(([skill, value]) => (
           <div
             key={`${category}-${skill}`}
             className="grid grid-cols-2 gap-x-2"
           >
-            {gamemasterView || isPlayer ? (
+            {gameMasterView || isPlayer ? (
               <>
                 <span className="text-gray-600 capitalize">
                   {showCategory && `${category} - `}
                   {skill.replace(/([A-Z])/g, " $1").trim()}:
                 </span>
-                <span className="font-mono text-right">{String(value)}</span>
+                <span className="text-right font-mono">{String(value)}</span>
               </>
             ) : null}
           </div>
-        ))
+        )),
       )}
     </div>
   );
--- frontend/src/components/CharactersTable/StatsTable.tsx
+++ frontend/src/components/CharactersTable/StatsTable.tsx
@@ -4,29 +4,29 @@ interface StatsTableProps {
   stats: Stats;
   className?: string;
   isPlayer: boolean;
-  gamemasterView: boolean;
+  gameMasterView: boolean;
 }
 
 export default function StatsTable({
   stats,
   className = "",
   isPlayer,
-  gamemasterView,
+  gameMasterView,
 }: StatsTableProps) {
-  if (!gamemasterView && !isPlayer) {
+  if (!gameMasterView && !isPlayer) {
     return <></>;
   }
 
   return (
     <div
-      className={`space-y-1 grid grid-cols-2 max-w-3xs gap-x-12 ${className}`}
+      className={`grid max-w-3xs grid-cols-2 space-y-1 gap-x-12 ${className}`}
     >
       {Object.entries(stats).map(([key, value]) => (
         <div key={key} className="grid grid-cols-2 gap-x-2">
           <span className="text-gray-600 uppercase">
             {STATS_TRANSLATION[key as keyof Stats] || key}
           </span>
-          <span className="font-mono text-right">{value}</span>
+          <span className="text-right font-mono">{value}</span>
         </div>
       ))}
     </div>
