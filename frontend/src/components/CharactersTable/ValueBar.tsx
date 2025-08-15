33794e2dafbd816205e40c943011477d08be7347
Nikodem Majzner <81244722+SAY10s@users.noreply.github.com>
2025-08-15 18:28:00 +0200

    FIX: correct percentage calculation and style width in ValueBar component

--- frontend/src/components/CharactersTable/ValueBar.tsx
+++ frontend/src/components/CharactersTable/ValueBar.tsx
@@ -13,14 +13,18 @@ export const ValueBar = ({
   height?: string;
   isPlayer?: boolean;
 }) => {
-  const percentage = (current / max) * 100;
+  let percentage = (current / max) * 100;
+  if (percentage <= 0) percentage = 0;
+  if (percentage >= 100) percentage = 100;
 
   return (
     <div className="flex w-full">
       <div className="border-border group relative w-full border-2 border-double">
         <div
           className={`${bgColor} m-0.5 ${height} transition-all duration-1000`}
-          style={{ width: `${percentage - 1}% ` }}
+          style={{
+            width: percentage >= 100 ? "calc(100% - 4px)" : `${percentage}%`,
+          }}
         ></div>
         {(isPlayer || gamemasterView) && (
           <div className="pointer-events-none absolute top-full left-1/2 z-10 -translate-x-1/2 rounded bg-black px-2 py-1 text-xs whitespace-nowrap text-white opacity-0 transition-opacity group-hover:opacity-100">
